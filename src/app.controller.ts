29a78d0d94ec7274c93dfa414e58126a9f912c44
Alexandre <amoutonbrady@gmail.com>
2020-03-22 20:25:39 +0100

    :tada: Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,54 @@
+import {
+	Controller,
+	Post,
+	UseInterceptors,
+	UploadedFile,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as Papa from 'papaparse';
+
+import { Open } from 'unzipper';
+import { AppService } from './app.service';
+
+const filter = [
+	'agency.txt',
+	'calendar_dates.txt',
+	'stop_times.txt',
+	'trips.txt',
+	'stops.txt',
+	'calendar.txt',
+	'routes.txt',
+];
+
+type UploadedFile = {
+	fieldname: string;
+	originalname: string;
+	encoding: string;
+	mimetype: string;
+	buffer: Buffer;
+	size: number;
+};
+
+@Controller()
+export class AppController {
+	constructor(private readonly appService: AppService) {}
+
+	@Post('upload')
+	@UseInterceptors(FileInterceptor('gtfs'))
+	async uploadFile(@UploadedFile() gtfs: UploadedFile): Promise<string> {
+		const directory = await Open.buffer(gtfs.buffer);
+
+		const tables = {};
+
+		for (const file of directory.files) {
+			if (!filter.includes(file.path)) continue;
+			const tableName = file.path.slice(0, file.path.length - 4);
+
+			tables[tableName] = await file
+				.buffer()
+				.then(buffer => buffer.toString('utf-8'));
+		}
+
+		return JSON.stringify(tables);
+	}
+}
